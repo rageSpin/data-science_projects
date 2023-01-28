a2f4db521efd35b6e147612dcbd6a2203a40ab3b
rageSpin <stefanokx97@gmail.com>
2023-01-28 13:34:48 +0100

    fixed out-of-index counter

--- GUIs/imgui/video_player.py
+++ GUIs/imgui/video_player.py
@@ -30,32 +30,40 @@ def format_counter(fc):
     # dpg.configure_item("slider", label=f"{hours}:{minutes:02}:{seconds:02}") 
     dpg.set_value('video_time', f"{hours}:{minutes:02}:{seconds:02}")
 
+def reset_state():
+    global vr
+    dpg.set_value('video_running', False)
+    format_counter(0)
+    del vr
+    dpg.set_value("video_texture", np.zeros(620*420*3))
+    dpg.set_value("frame_progress", 0)
+
 raw_data = np.zeros(620*420*3)
 
 with dpg.texture_registry(show=False):
     dpg.add_raw_texture(width=620, height=420, default_value=raw_data, format=dpg.mvFormat_Float_rgb, tag="video_texture")
 
 def change_text(sender, app_data):
-    it_conf = dpg.get_item_configuration("progress_bar")
-    pos = dpg.get_item_state("progress_bar")['pos']
-    # print(it_conf['width'], it_conf['height'])
-    # print(pos)
-    # print(dpg.get_mouse_pos(local=False))
-    # print(dpg.get_mouse_pos(local=True))
-    # print(dpg.get_item_state("video_texture"))
-    mouse_pos = dpg.get_mouse_pos(local=False)
-    min_y = pos[1] + 20
-    max_y = pos[1] + it_conf['height'] + 20
-    min_x = pos[0]
-    max_x = pos[0] + it_conf['width']
-
-    if mouse_pos[1] >= min_y and mouse_pos[1] <= max_y:
-        if mouse_pos[0] >= min_x and mouse_pos[0] <= max_x:
-            print("inside progress bar")
-            norm_prog = (mouse_pos[0]-min_x)/(max_x-min_x)
-            #dpg.configure_item('progress_bar', default_value=norm_prog)
-            dpg.set_value('frame_progress', norm_prog)
-            #dpg.set_value("frame_counter", int(dpg.get_item_configuration('slider')['max_value']*norm_prog))
+    if dpg.get_value('video_running'):
+        it_conf = dpg.get_item_configuration("progress_bar")
+        pos = dpg.get_item_state("progress_bar")['pos']
+        # print(it_conf['width'], it_conf['height'])
+        # print(pos)
+        # print(dpg.get_mouse_pos(local=False))
+        # print(dpg.get_mouse_pos(local=True))
+        # print(dpg.get_item_state("video_texture"))
+        mouse_pos = dpg.get_mouse_pos(local=False)
+        min_y = pos[1] + 20
+        max_y = pos[1] + it_conf['height'] + 20
+        min_x = pos[0]
+        max_x = pos[0] + it_conf['width']
+        if mouse_pos[1] >= min_y and mouse_pos[1] <= max_y:
+            if mouse_pos[0] >= min_x and mouse_pos[0] <= max_x:
+                print("inside progress bar")
+                norm_prog = (mouse_pos[0]-min_x)/(max_x-min_x)
+                #dpg.configure_item('progress_bar', default_value=norm_prog)
+                dpg.set_value('frame_progress', norm_prog)
+                #dpg.set_value("frame_counter", int(dpg.get_item_configuration('slider')['max_value']*norm_prog))
 
 with dpg.handler_registry():
     dpg.add_mouse_click_handler(callback=change_text)
@@ -74,8 +82,8 @@ with dpg.window(pos=(0,0), width=705, height=560, show=True):
         dpg.add_text("", tag='video_time', wrap=0)
 
 # only for fast testing
-# dpg.set_value("video_filepath", "C:\\Users\\stefano.giannini_ama\\Videos\\GUI_video-demo_coherent-interaction.mp4")
-# dpg.set_value("video_running", True)
+dpg.set_value("video_filepath", "C:\\Users\\stefano.giannini_ama\\Videos\\GUI_video-demo_coherent-interaction.mp4")
+dpg.set_value("video_running", True)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
@@ -90,17 +98,19 @@ while dpg.is_dearpygui_running():
             _update_textures(frame_counter)
             dpg.set_value('frame_progress', dpg.get_value('frame_progress')+step)
             format_counter(frame_counter)
+            # if frame_counter%60 == 0: print(dpg.get_frame_rate())
+            if (frame_counter+1) == video_len: reset_state()
+
         except Exception as e:
             vr = decord.VideoReader(dpg.get_value('video_filepath'), width=620, height=420)
             step = 1/len(vr)
             video_len = len(vr)
             # print(video_len, step)
             print(e)
-            # print(vr.get_avg_fps())
+            print("FPS", vr.get_avg_fps())
             #dpg.configure_item("slider", max_value=len(vr))
 
-    time.sleep(1/(dpg.get_value('fps')*2))
-    # if frame_counter%60 == 0: print(dpg.get_frame_rate())
+    # time.sleep(1/(dpg.get_value('fps')*2))
     dpg.render_dearpygui_frame()
 
 

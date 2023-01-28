c91df8a7d4ca813599e3ea77ea82cd7563bab372
rageSpin <stefanokx97@gmail.com>
2023-01-28 19:07:16 +0100

    solved bezier issue with bezier precision

--- GUIs/kivy/draw.py
+++ GUIs/kivy/draw.py
@@ -1,24 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color
+from kivy.graphics import Color, ClearColor
 from kivy.graphics import Line, Ellipse, Bezier
 
 
 class Draw(Widget):
-    def __init__(self, **kwargs):
+    def __init__(self, loop=False,**kwargs):
         super(Draw, self).__init__(**kwargs)
+        self.loop = loop
         self.bind(pos=self.update_canvas)
         self.bind(size=self.update_canvas)
         self.update_canvas()
 
     def update_canvas(self, *args):
         self.canvas.clear()
-        with self.canvas:
-            Color(0.0, 1., 0.0, .5, mode='rgba')
-            # self.line1 = Line(points=[200, 100, 100, 200, 350, 250], width=3)
-            #Color(0.0, 0.0, 1., .5, mode='rgba')
+        with self.canvas.before:
+            Color(0.0, 1., 0.0, 0.5)
+            self.line1 = Line(points=[200, 100, 100, 200, 350, 250], width=3)
+            # ClearColor(1,1,1,1)
+            c=Color(b=1, a=0.4, r=0, g=0)
+            # Line(circle=(150, 150, 50, 90, 180, 20), width=4)
             #Ellipse(pos=self.pos, size=(20,20))
-            Line(points=(10,10), bezier=[0, 0, 100, 150, 30, 50, 200, 220], width=3)
+            Line(bezier=(200, 100, 250, 150, 300, 50, 350, 100), width=5, dash_length=100, dash_offset=10, bezier_precision=100)
+            # Bezier(points=[0, 0, 100, 150, 30, 50, 200, 220], width=5, dash_length=100, dash_offset=10)
 
 
 class MyApp(App):
